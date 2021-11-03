a2bdde58a432fce6a4233eac5863174c82df20a2
emeraldloop <sergey.nikishin.2000@mail.ru>
2021-11-03 21:46:42 +0300

    Добавьте файлы проекта.

---
+++ LR1_zadanie2/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace LR1_zadanie2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(615, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Математическое ожидание";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(74, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Расчёт дисперсии";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 38);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "СКО";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(369, 143);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор распределения";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(22, 57);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(100, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Равномерное";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(22, 23);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(96, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Нормальное";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(245, 342);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 43);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Открыть файл с данными";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 436);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ LR1_zadanie2/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LR1_zadanie2
+{
+    public partial class Form1 : Form
+    {
+        private List<double> data; // данные для обработки
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e) // вывод диспресии на экран
+        {
+            if (data == null)
+            {
+                MessageBox.Show("Загрузите данные!", "Ошибка!");
+                return;
+            }
+            MessageBox.Show("Дисперсия равна " + countD(),"Расчёт");
+        }
+        private double countD() // расчет диспресии
+        {
+            double D=0;
+            if (radioButton1.Checked == true) // нормальное распределение
+            {
+                double sigma = 0;
+                foreach (float a in data)
+                    sigma += Math.Pow(a - data.Average(), 2);
+                D = sigma / (data.Count - 1);
+            }
+            if (radioButton2.Checked == true) // равномерное распределение
+            {
+                D = Math.Pow(data.Max() - data.Min(), 2) / 12;
+            }
+            return Math.Round(D,5);
+        }
+
+        private void button3_Click(object sender, EventArgs e) // вывод СКО на экран
+        {
+            if (data == null)
+            {
+                MessageBox.Show("Загрузите данные!","Ошибка!");
+                return;
+            }
+            MessageBox.Show("СКО равно "+countSKO(), "Расчёт");
+        }
+        private double countSKO() // расчет СКО
+        {
+            double SKO;
+            SKO = Math.Sqrt(countD());
+            return Math.Round(SKO, 5);
+        }
+        private void button4_Click(object sender, EventArgs e) // загрузка данных
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            // читаем файл в строку
+            string fileText = System.IO.File.ReadAllText(filename);
+            data = new List<double>(); // данные для обработки
+            for (int i=0;i<fileText.Length;i++) 
+            {
+                if(fileText[i]==' ')
+                { 
+                    StringBuilder number=new StringBuilder();
+                    for (int j = 0; j < i; j++)
+                        number.Append(fileText[j]);
+                    data.Add(Convert.ToDouble(number.ToString()));
+                    fileText=fileText.Remove(0, i);
+                    i = 0;
+                }
+            }
+            MessageBox.Show("Файл открыт");
+        }
+        private void button1_Click(object sender, EventArgs e) // Вывод мат ожидания на экран
+        {
+            if(data==null)
+            {
+                MessageBox.Show("Загрузите данные!", "Ошибка!");
+                return;
+            }
+            MessageBox.Show("Математическое ожидание равно " + countM(),"Расчёт");
+        }
+        
+        private double countM() // расчёт мат ожидания
+        {
+            double M=0;
+            if (radioButton1.Checked == true) //нормальное распределение
+            {
+                M = data.Average();
+            }
+            if (radioButton2.Checked == true) // равномерное распределение
+            {
+                M = (data.Max() - data.Min()) / 2;
+            }
+            return Math.Round(M, 5);
+        } 
+    }
+}
